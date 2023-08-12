96b79fdf745f6f5db0026cfb878afb361cfcae11
zaida04 <nico.03727+github@gmail.com>
2023-08-12 17:21:21 -0400

    fix: ficon

--- apps/landing/src/pages/_app.tsx
+++ apps/landing/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/globals.css'
 import type { AppProps } from 'next/app'
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
