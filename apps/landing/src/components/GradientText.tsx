7028db01e4c5b3c0edea996be0421fa087e8d4d8
zaida04 <nico.03727+github@gmail.com>
2023-08-12 23:36:28 -0400

    chore: make pointer default

--- apps/landing/src/components/GradientText.tsx
+++ apps/landing/src/components/GradientText.tsx
@@ -1,5 +1,5 @@
 export default function GradientText(props: { from: string; via: string; to: string; children: React.ReactNode; }) {
-    return <span className={`hover:text-transparent bg-clip-text bg-gradient-to-r ${props.from} ${props.via} ${props.to} transition-all duration-300 ease-in-out hover-animate-gradient`}>
+    return <span className={`hover:text-transparent bg-clip-text bg-gradient-to-r ${props.from} ${props.via} ${props.to} transition-all duration-300 ease-in-out hover-animate-gradient hover:cursor-default`}>
         {props.children}
     </span>
 }
